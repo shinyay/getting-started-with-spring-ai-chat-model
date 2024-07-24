53235d019571fd58c93e794a7e8bd8ec38c072c8
shinyay <shinya.com@gmail.com>
2024-07-24 10:17:30 +0900

    Modify typo

--- src/test/java/com/microsoft/shinyay/ai/demo/DemoApplicationTests.java
+++ src/test/java/com/microsoft/shinyay/ai/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.microsoft.shinyay.ai.demo.demo;
+package com.microsoft.shinyay.ai.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
